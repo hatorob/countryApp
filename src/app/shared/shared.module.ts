763f2190989fe067da6ff2b1925e3bc72542f977
Hector Toro <alejotoro94@hotmail.com>
2024-03-05 11:32:15 -0500

    Se agrega page contact

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,16 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent,
     AboutPageComponent,
-    SidebarComponent
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
   ],
   imports: [
     CommonModule,
@@ -19,6 +21,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
     SidebarComponent,
   ]
