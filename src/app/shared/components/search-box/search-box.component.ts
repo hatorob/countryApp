f51f45566d692b24103ee214b98d080ccf84e600
Hector Toro <alejotoro94@hotmail.com>
2024-03-11 15:23:47 -0500

    Se cambia el enter event por un observable de tipo Subject, que es nos permitira detectar cuando el usuario deje de escribir y ejecuta el evento de busqueda

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  // Subject viene de rxjs -> reactivo
+  private debouncer: Subject<string> = new Subject<string>();
 
   //! creamos nuestro ViewChild para obtener el valor
   @ViewChild('txtInput')
@@ -18,11 +22,28 @@ export class SearchBoxComponent {
   @Output()
   public searchByCapital: EventEmitter<string> = new EventEmitter();
 
-  public emitEventInput = (): void => {
+  // Lo usamos para la implementación de subject
+  ngOnInit(): void {
+    this.debouncer
+        .pipe(
+          debounceTime(1000)
+        )
+        .subscribe( value => {
+          this.searchByCapital.emit(value);
+        })
+  }
+
+
+  /* public emitEventInput = (): void => {
+    //! Ya no lo usamos para el emit
     let searchCapital: string = this.txtInput.nativeElement.value;
     console.log("desde searchBox",searchCapital);
     this.searchByCapital.emit(searchCapital);
     this.txtInput.nativeElement.value = "";
+  } */
+
+  public onKeyPress = ( searchTerm: string ) => {
+    this.debouncer.next( searchTerm );
   }
 
 }
