827cb692d26d82c591b761eca40ad51da406d305
Hector Toro <alejotoro94@hotmail.com>
2024-03-11 11:38:08 -0500

    Se agrega el componente de loading y su funcionalidad en capital page

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -9,17 +9,20 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: Boolean = false;
 
   constructor( private servicesCountry: CountriesService ) {
 
   }
 
   public searchByCapital = (term: string): void => {
-    console.log("desde by capital page", {term});
+    //console.log("desde by capital page", {term});
+    this.isLoading = true;
+
     this.servicesCountry.searchByCapital(term).subscribe( contries => {
       this.countries = contries;
+      this.isLoading = false;
     });
-    console.log("countries", this.countries);
   }
 
 }
