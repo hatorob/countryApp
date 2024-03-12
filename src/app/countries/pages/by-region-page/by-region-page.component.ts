a8d042576418d0d75ba05ef90920e8487306fc57
Hector Toro <alejotoro94@hotmail.com>
2024-03-12 18:47:56 -0500

    Se cambia el searchbox por botones según las regiones disponibles

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,6 +12,7 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
   public isLoading: Boolean = false;
+  public regions: Region[] = ['Africa','Americas','Asia','Europe','Oceania'];
 
   constructor( private serviceCountries: CountriesService ) {
 
